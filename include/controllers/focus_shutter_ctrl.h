2b0eef5e427c1bdb6672811001cfdd110ab5353d
Jack Damiano <jack.damiano@gmail.com>
2021-09-16 11:00:47 -0600

    added controller .h and .c for focus/shutter

---
+++ include/controllers/focus_shutter_ctrl.h
@@ -0,0 +1,32 @@
+/**
+ * \file
+ * \date 16 September 2021
+ * \author Jack Damiano
+ * 
+ * \brief Controller for the focus and shutter on the cameras.
+ * 
+ * Each board has an output for the focus and shutter of the cameras.
+ * This controller incorporates delays and duty cycles to facilitate
+ * the focus/shutter process.
+ * 
+ */
+
+#ifndef FOCUS_SHUTTER_CTRL_H
+#define FOCUS_SHUTTER_CTRL_H
+
+#include<stdint.h>
+#include<stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//function declarations go here
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/controllers/focus_shutter_ctrl.c
@@ -0,0 +1,10 @@
+#include<math.h>
+#include<stdint.h>
+
+#include<utils/utils.h>
+
+#include<drivers/pwm.h>
+#include<port.h>
+#include<drivers/ws2812.h>
+#include<controllers/focus_shutter_ctrl.h>
+
